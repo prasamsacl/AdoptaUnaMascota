049a721c54227a463304d58086a1840e0e4f18c6
prasamsacl <113896447+prasamsacl@users.noreply.github.com>
2023-05-07 11:25:42 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$dbname = "AdoptaUnaMascota";
+
+$db = new mysqli($servername, $username, $password, $dbname);
+// Verificar si la conexión fue exitosa
+if ($db->connect_error) {
+  die("Conexión fallida: " . $db->connect_error);
+}
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  // Consulta a la base de datos para verificar las credenciales
+  $sql = "SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
+  $result = $db->query($sql);
+  // Verificar si se encontró un resultado
+  if ($result->num_rows == 1) {
+    // Si las credenciales son válidas, redirigir al usuario a la página de inicio
+    header('Location: login.php');
+    exit;
+  } else {
+    // Si las credenciales no son válidas, redirigir al usuario a la página de inicio de sesión con un mensaje de error
+    header('Location: login.php?error=1');
+    exit;
+  }
+}
+// Cerrar la conexión a la base de datos
+$db->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Iniciar Sesion</title>
+  <style>
+    body {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h1>Iniciar Sesion</h1>
+
+  <form action="" method="post">
+    <label for="email">Email:</label><br>
+    <input type="text" name="email" placeholder="Email" required><br><br>
+
+    <label for="password">Contraseña:</label><br>
+    <input type="password" name="password" placeholder="Contraseña" required><br><br>
+
+    <input type="submit" value="Iniciar Sesion"><br><br>
+
+    <p>¿No tienes una cuenta?</p>
+    <a href="registro.php">Registrarse</a>
+
+    <?php
+    // Mostrar un mensaje de error si las credenciales son inválidas
+    if (isset($_GET['error']) && $_GET['error'] == 1) {
+      echo "<p style='color:red'>Credenciales inválidas. Por favor, inténtelo de nuevo.</p>";
+    }
+    ?>
+  </form>
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de Registro</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        form {
+            width: 400px;
+            padding: 20px;
+            background-color: #f0f0f0;
+            border-radius: 5px;
+        }
+
+        label, input {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+
+     <?php
+      $server = "localhost";
+      $username = "root"; // Reemplazar con el nombre de usuario de la base de datos
+      $password = "12345"; // Reemplazar con la contraseña de la base de datos
+      $database = "AdoptaUnaMascota"; // Reemplazar con el nombre de la base de datos
+
+      // Crear la conexión con la base de datos
+      $db = mysqli_connect($server, $username, $password, $database);
+
+      // Verificar si la conexión es exitosa
+      if (!$db) {
+        die("La conexión a la base de datos ha fallado: " . mysqli_connect_error());
+      }
+
+      // Si se envió el formulario, insertar los datos en la base de datos
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $email = $_POST["email"];
+        $pais = $_POST["pais"];
+        $provincia = $_POST["provincia"];
+        $contraseña = $_POST["contraseña"];
+
+        $sql = "INSERT INTO usuarios (nombre, apellidos, email, pais, provincia, contraseña) VALUES ('$nombre', '$apellidos', '$email', '$pais', '$provincia', '$contrasena')";
+
+        if (mysqli_query($db, $sql)) {
+          echo "El registro se ha completado satisfactoriamente";
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }
+      }
+
+      // Cerrar la conexión con la base de datos
+      mysqli_close($db);
+    ?>
+    <h1>Formulario de Registro</h1>
+    <form action="registrar.php" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+
+        <label for="apellidos">Apellidos:</label>
+        <input type="text" id="apellidos" name="apellidos" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="confirmar_email">Confirmar Email:</label>
+        <input type="email" id="confirmar_email" name="confirmar_email" required>
+
+        <label for="pais">País:</label>
+        <input type="text" id="pais" name="pais" required>
+
+        <label for="provincia">Provincia:</label>
+        <input type="text" id="provincia" name="provincia" required>
+
+        <label for="contraseña">Contraseña:</label>
+        <input type="password" id="contraseña" name="contraseña" required>
+
+        <label for="confirmar_contraseña">Confirmar Contraseña:</label>
+        <input type="password" id="confirmar_contraseña" name="confirmar_contraseña" required>
+
+        <input type="submit" name="submit" value="Crear Cuenta">
+    </form>
+</body>
+</html>
+
