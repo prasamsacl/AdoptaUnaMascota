e55410bb66940225a1b1dd3e2c6a83f857ff092b
prasamsacl <113896447+prasamsacl@users.noreply.github.com>
2023-06-11 20:41:32 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,20 +10,41 @@ $db = new mysqli($servername, $username, $password, $dbname);
 if ($db->connect_error) {
   die("Conexión fallida: " . $db->connect_error);
 }
+//crear la conexion 
+//$hash = Password::hash('micontraseña');
+//comprobar la contraseña introducida 
+//if(Password::verify('micontraseña',$hash)){
+//echo 'contraseña correcta!\n';
+//}else{
+// echo 'contraseña incorrecta!\n';
+//}
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST['email'];
   $password = $_POST['password'];
+
+  // Encriptar la contraseña
+  $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
   // Consulta a la base de datos para verificar las credenciales
-  $sql = "SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
+  $sql = "SELECT * FROM usuarios WHERE email = '$email'";
   $result = $db->query($sql);
+
   // Verificar si se encontró un resultado
   if ($result->num_rows == 1) {
-    // Si las credenciales son válidas, redirigir al usuario a la página de inicio
-    header('Location: login.php');
-    exit;
+    $row = $result->fetch_assoc();
+    // Verificar si las credenciales son válidas
+    if (password_verify($password, $row['password'])) {
+      // Si las credenciales son válidas, redirigir al usuario a la página de inicio
+      header('Location: login.php');
+      exit;
+    } else {
+      // Si las credenciales no son válidas, redirigir al usuario a la página de inicio de sesión con un mensaje de error
+      header('Location: login.php?error=1');
+      exit;
+    }
   } else {
-    // Si las credenciales no son válidas, redirigir al usuario a la página de inicio de sesión con un mensaje de error
+    // Si el usuario no existe, redirigir al usuario a la página de inicio de sesión con un mensaje de error
     header('Location: login.php?error=1');
     exit;
   }
